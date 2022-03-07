5d05aa08bbbd72d13aedf0e30a57a0cb17a77d26
Shamsul Arafin Mahtab <arafinmahtab@gmail.com>
2022-03-07 13:12:27 +0600

    Upgrade build gradle plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,50 +36,53 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = rootProject.extra["compose_version"] as String
+        kotlinCompilerExtensionVersion = rootProject.extra["composeVersion"] as String
+    }
+    kapt {
+        correctErrorTypes = true
     }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.0")
-    implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
-    implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
-    implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
-    implementation("androidx.compose.runtime:runtime-livedata:${rootProject.extra["compose_version"]}")
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("androidx.compose.ui:ui:${rootProject.extra["composeVersion"]}")
+    implementation("androidx.compose.material:material:${rootProject.extra["composeVersion"]}")
+    implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["composeVersion"]}")
+    implementation("androidx.compose.runtime:runtime-livedata:${rootProject.extra["composeVersion"]}")
     implementation("androidx.activity:activity-compose:1.4.0")
-    implementation("androidx.navigation:navigation-compose:2.4.0-rc01")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
+    implementation("androidx.navigation:navigation-compose:2.4.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
 
-    implementation("com.google.accompanist:accompanist-insets:${rootProject.extra["accompanist_version"]}")
+    implementation("com.google.accompanist:accompanist-insets:0.18.0")
 
-    implementation("com.google.android.material:material:${rootProject.extra["material_version"]}")
+    implementation("com.google.android.material:material:1.5.0")
 
-    implementation("com.google.code.gson:gson:2.8.9")
-    implementation("com.google.dagger:hilt-android:${rootProject.extra["hilt_version"]}")
-    kapt("com.google.dagger:hilt-android-compiler:${rootProject.extra["hilt_version"]}")
+    implementation("com.google.code.gson:gson:2.9.0")
+    implementation("com.google.dagger:hilt-android:${rootProject.extra["hiltVersion"]}")
+    kapt("com.google.dagger:hilt-android-compiler:${rootProject.extra["hiltVersion"]}")
 
-    implementation("com.squareup.retrofit2:retrofit:${rootProject.extra["retrofit2_version"]}")
-    implementation("com.squareup.retrofit2:converter-gson:${rootProject.extra["retrofit2_version"]}")
+    implementation("com.squareup.retrofit2:retrofit:${rootProject.extra["retrofit2Version"]}")
+    implementation("com.squareup.retrofit2:converter-gson:${rootProject.extra["retrofit2Version"]}")
 
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
 
-    implementation("com.airbnb.android:lottie-compose:4.2.2")
+    implementation("com.airbnb.android:lottie-compose:5.0.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["compose_version"]}")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["composeVersion"]}")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    val kotlin_version by extra("1.5.31")
-    val compose_version by extra("1.0.5")
-    val hilt_version by extra("2.40.5")
-    val retrofit2_version by extra("2.9.0")
-    val material_version by extra("1.4.0")
-    val accompanist_version by extra("0.18.0")
+plugins {
+    id("com.android.application") version "7.1.2" apply false
+    id("com.android.library") version "7.1.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.6.10" apply false
+}
 
-    repositories {
-        google()
-        mavenCentral()
-    }
+buildscript {
+    val composeVersion by extra("1.1.0")
+    val hiltVersion by extra("2.41")
+    val retrofit2Version by extra("2.9.0")
+
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.4")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:$hilt_version")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:$hiltVersion")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
